a739c93a4d0d53d61c6c956b7ae5b8a7ef925099
feedcase <seva1502@gmail.com>
2021-06-04 02:08:21 +0300

    fixes

--- kwp/settings.py
+++ kwp/settings.py
@@ -111,7 +111,8 @@ CACHES = {
         'LOCATION': f'redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB_ID}',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+        },
+        'KEY_PREFIX': 'kiwapower'
     }
 }
 

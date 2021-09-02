091b7ea10145f13d656bee016cc897b1f02a8381
smov99 <smorozov@gmail.com>
2021-09-01 20:24:45 -0400

    correction new sandbox settings

--- kwp/ansible_settings.py
+++ kwp/ansible_settings.py
@@ -7,6 +7,8 @@ SECRET_KEY = 'at#96-#7p!4bzqreegk@8e&*8whx^ciky5t_o)x04fti31r%8h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CSRF_TRUSTED_ORIGINS = ['127.0.0.1']
+
 ALLOWED_HOSTS = [
     '*',
 ]
@@ -40,35 +42,29 @@ CACHES = {
     }
 }
 
-# Salesforce API
-SF_CONSUMER_SECRET = 'CD74EE798586E68841BF6CFC60B61B451449904ACDFA3B9E6457D852954FF6B9'
-SF_CONSUMER_KEY = '3MVG9M43irr9JAuyX9k4MSEg2sKpL9Eswc9hslkN7vnqYMZxFkIUreh4BiO3HhC..dq9GsWnIqebJDxzQ0CYO'
-SF_INITIAL_ACCESS_TOKEN = 'Geosoft999Cel800D040000008gMG88804000000GndYOHyk2gVh5wcpKjg0OKD7ZhkgtKCImZvHknjlwpDw5RxgucxygvBFwAVS0qlk6taEnGWSQTDx'
-SF_USER_NAME = 'smorozov@gmail.com'
+SF_CONSUMER_SECRET = '6687B3551B44B14613450674FD6D2F5948B206FB0F6872E5E0E07617D9482299'
+SF_CONSUMER_KEY = '3MVG9AJuBE3rTYDgjVgTfUitelQPY_J9j15W4eFxHPuOoTmShEk3gN82WbE2Bu68qMyP5vj_bILwpnS2nclWh'
+SF_USER_NAME = 'django@kiwapower.com.july16'
 SF_API_VERSION = 'v51.0'
-SF_PASSWORD = '_geosoft99'
+SF_PASSWORD = 'Ge0s0ft#99'
 SF_LOGIN_URL = 'https://test.salesforce.com/services/oauth2/token'
-SF_USER_ID = '0056g0000036ZdTAAU'
-
+SF_USER_ID = '0055x00000BFIezAAH'
 TRUSTED_EMAIL = 'backdoor@email.com'
 
 # IP geolocation
 IPDATA_TOKEN = '5d721175bef6ff7756ec1ef843876cbf5e3559fb2f1c051f46ff9dec'
 
 # stage 2
-env = dev
+ENV = 'dev'
 
 # S3
-kwp_s3: kwp-001
-kwp_aws_key: AKIAZXQ6BWHIYPRDXQE7
-kwp_aws_secret: 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
-kwp_s3_resources: /dev/resources/
-kwp_s3_proposal-docs: /dev/proposal-docs/
+KWP_S3 = 'kwp-001'
+KWP_AWS_KEY = 'AKIAZXQ6BWHIYPRDXQE7'
+KWP_AWS_SECRET = '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'
+KWP_S3_RESOURCES = '/dev/resources/'
+KWP_S3_PROPOSAL_DOCS = '/dev/proposal-docs/'
 
 # list of resources, non case sensitive
-static_resources:
-  - show_case_study__c
-  - show_quick_start_guide__c
-  - show_brochure__c
+STATIC_RESOURCES = ['show_case_study__c', 'show_quick_start_guide__c', 'show_brochure__c']
 #  - show_kiwapower_at_a_glance_video__c    # commented for the following testing
 

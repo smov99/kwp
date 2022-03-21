5caf6c669a9095473262c595edea01346d99d7fb
smov99 <smorozov@gmail.com>
2022-03-21 19:03:02 -0400

    Revert "Fixes"
    This reverts commit 8384dff9f2aa7704c78c2ed3f9c52f75848bd6e5.

--- proposal/models.py
+++ proposal/models.py
@@ -1,6 +1,9 @@
+import os
+
 from django.db import models
 
 import proposal.services as services
+import kwp.settings as settings
 
 
 class BaseModel(models.Model):
@@ -95,13 +98,30 @@ class StaticResource(BaseModel):
 
     def __init__(self, *args, **kwargs):
         super(StaticResource, self).__init__(*args, **kwargs)
-        self.__original_document_name_en = self.document_en.name
-        self.__original_document_name_es = self.document_es.name
+        self.__original_document_name = self.document.name
 
     def __str__(self):
         return self.file_description
 
     def save(self, *args, **kwargs):
-        self = services.save_document(self, "en", self.__original_document_name_en)
-        self = services.save_document(self, "es", self.__original_document_name_es)
+        if self.document:
+            if self.document.name != self.__original_document_name:
+                self.s3_file_location = f'{settings.KWP_S3_RESOURCES}{self.document.name}'
+                try:
+                    services.write_file_in_memory(self.document.path, self.document.file)
+                except:
+                    os.remove(self.document.path)
+                    services.write_file_in_memory(self.document.path, self.document.file)
+                services.s3_upload_file(self.document.name, 'static')
+                os.remove(self.document.path)
+            else:
+                try:
+                    services.s3_download_file(self.document.name, 'static')
+                except FileExistsError:
+                    pass
+        else:
+            self.is_active = False
+            if self.s3_file_location:
+                services.s3_delete_static_file(self.s3_file_location)
+            self.s3_file_location = None
         super(StaticResource, self).save(*args, **kwargs)
